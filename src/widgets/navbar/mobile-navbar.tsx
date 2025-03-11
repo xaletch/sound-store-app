10a94c721832d8ae56a9356261f5e480b2072459
Xaletch <kiril.kolesnikov0609@mail.ru>
2025-03-11 11:31:15 +0300

    navbar

--- src/widgets/navbar/mobile-navbar.tsx
+++ src/widgets/navbar/mobile-navbar.tsx
@@ -1,5 +1,5 @@
 import { FavouritesIcon, FoldersIcon, HomeIcon, SearchIcon } from "@/shared/icons"
-import { Link } from "@tanstack/react-router"
+import { Link, useLocation } from "@tanstack/react-router"
 
 const items = [
   {
@@ -25,14 +25,16 @@ const items = [
 ]
 
 export const MobileNavbar = () => {
+  const location = useLocation().pathname;
+
   return (
     <div className="fixed bottom-0 left-0 w-full z-40 border-t border-black bg-[#E7E4DD]">
       <div className="py-1 px-5 grid grid-cols-4 gap-4 items-center">
         {items.map((item, index) => (
           <div key={index} className="flex flex-col">
             <Link to={item.href}>
-              <div className="w-[35px] mx-auto">{item.icon}</div>
-              <div className="text-xs font-medium text-center">{item.name}</div>
+              <div className={`w-[35px] mx-auto ${location === item.href ? 'icon-active' : ''}`}>{item.icon}</div>
+              <div className={`text-xs font-medium text-center ${location === item.href ? 'text-[#436c5f]' : ''}`}>{item.name}</div>
             </Link>
           </div>
         ))}
