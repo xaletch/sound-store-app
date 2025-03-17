d2445d7c6050d4c565d9025a259433cce2e9115e
Xaletch <kiril.kolesnikov0609@mail.ru>
2025-03-17 16:32:56 +0300

    update IWebApp types for BackButton

--- src/app/providers/telegram/telegram.types.ts
+++ src/app/providers/telegram/telegram.types.ts
@@ -35,10 +35,10 @@ export interface IWebApp {
   headerColor: string;
   backgroundColor: string;
   BackButton: {
-    hide(): unknown;
-    show(): void;
-    isVisible: boolean;
-    onClick(): unknown;
+    show: () => void;
+    hide: () => void;
+    onClick: (callback: () => void) => void;
+    offClick: (callback: () => void) => void;
   };
   MainButton: {
     text: string;
