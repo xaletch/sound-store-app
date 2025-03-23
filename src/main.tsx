edbadf82676ca9497d50c44a33e9cf4688e0df73
Xaletch <kiril.kolesnikov0609@mail.ru>
2025-03-23 18:49:03 +0300

    init telegram sdk

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import './app/styles/index.css'
 import App from './app/App.tsx'
 import { Provider } from './app/providers/index.tsx'
 
+import { init } from '@telegram-apps/sdk-react';
+
+init();
+
 createRoot(document.getElementById('root')!).render(
   <Provider>
     <App />
