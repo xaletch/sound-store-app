40c6340430c089819cccd02fbbfb5d951576b84c
Xaletch <kiril.kolesnikov0609@mail.ru>
2025-03-26 16:40:21 +0300

    fixed ui bugs

--- src/features/referral-system/ui/index.tsx
+++ src/features/referral-system/ui/index.tsx
@@ -12,7 +12,7 @@ export const Referral = () => {
 
   return (
     <div className="w-full bg-[#7cc0ab] border border-black rounded-3xl" onClick={() => copy('Здесь могла быть ваша ссылка')}>
-      <div className="px-3.5 py-1.5 font-medium text-xs md:text-base">поделись ссылкой с другом и получи <span className="">10</span> кредитов</div>
+      <div className="px-1.5 380:px-3.5 py-1.5 font-medium text-[10px] 340:text-[11px] 380:text-xs md:text-base">поделись ссылкой с другом и получи <span className="">10</span> кредитов</div>
     </div>
   )
 }
