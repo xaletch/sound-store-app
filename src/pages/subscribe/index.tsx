65a22cad1c1e2c147e3c1785e0bf914f0935bf36
Xaletch <kiril.kolesnikov0609@mail.ru>
2025-05-20 19:26:38 +0300

    my subscribe

--- src/pages/subscribe/index.tsx
+++ src/pages/subscribe/index.tsx
@@ -33,6 +33,7 @@ export const Subscribe = () => {
       <Shadow cl="w-62 -translate-x-2/4 h-62 1080:w-104 1080:h-104  blur-[120px] 1080:blur-[200px] left-20 -top-16 1080:-top-10 1080:left-134" />
       <div className={`px-4 pb-4 relative z-[2] h-full ${!isPlayer ? 'pb-25' : ''}`}>
         <h1 className="text-center text-xl font-medium">{activity?.Credits === 0 ? 'Тарифы' : 'Ваш тариф'}</h1>
+        
         {subscribeLoad || activeSubLoad ? <SubscribeLoading /> : <SubscribeContent />}
       </div>  
     </>
